8fd3c11de192ee3a74a0e98b463d3005d1f3eeb0
Ahlemmer <ahlemmerabtene@gmail.com>
2024-10-29 15:26:58 +0100

    test

--- erp/conftest.py
+++ erp/conftest.py
@@ -1,2 +1,6 @@
-from erp.corelib.tests.fixtures import *  # noqa: F401, F403, E402
+
+import os
+
+os.environ['PYTEST_RUNNING'] = 'true'
+from erp.general.tests.fixtures import *  # noqa: F401, F403, E402
 from erp.user.tests.fixtures import *  # noqa: F401, F403, E402
--- erp/core/settings/__init__.py
+++ erp/core/settings/__init__.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from split_settings.tools import include, optional
 
+from erp.general.utils.pytest import is_pytest_running
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 ENVVAR_SETTINGS_PREFIX = "CORESETTING_"
@@ -10,7 +12,7 @@ ENVVAR_SETTINGS_PREFIX = "CORESETTING_"
 LOCAL_SETTINGS_PATH = os.getenv(f"{ENVVAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH")
 if not LOCAL_SETTINGS_PATH:
     # We dedicate local/settings.unittests.py to have reproducible unittest runs
-    LOCAL_SETTINGS_PATH = "local/settings.dev.py"
+    LOCAL_SETTINGS_PATH = f'local/settings{".unittests" if is_pytest_running() else ".dev"}.py'
 
 if not os.path.isabs(LOCAL_SETTINGS_PATH):
     LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
@@ -24,3 +26,5 @@ include(
     'logging.py',
     'rest_framework.py'
 )
+if not is_pytest_running():
+    assert SECRET_KEY is not NotImplemented  # type: ignore # noqa: F821
