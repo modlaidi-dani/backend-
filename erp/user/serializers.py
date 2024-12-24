cbdd1dfc5905e0cf683f23742f2e939a748b3605
diva <95laidi@gmail.com>
2024-12-24 15:34:54 +0100

    admin

--- erp/user/serializers.py
+++ erp/user/serializers.py
@@ -32,14 +32,24 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def to_internal_value(self, data):
         user=self.context['request'].user
         internal_value = super().to_internal_value(data)
-        
+        Username=internal_value["username"]
+        Password=internal_value["password"]
+        first_name=internal_value["first_name"]
+        last_name=internal_value["last_name"]
+        email=internal_value["email"]
+        datauser=dict(
+            username=Username,
+            password=Password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email, 
+        )
         # Récupérer le mot de passe et le hacher
-        if 'password' in internal_value:
-            password = internal_value.pop('password')
+        if 'password' in datauser:
+            password = datauser.pop('password')
             # Hacher le mot de passe
-            user = User(**internal_value)
+            user = User(**datauser)
             user.set_password(password)
-            internal_value['password'] = user.password  # Conserver le mot de passe haché
-
+            datauser['password'] = user.password  # Conserver le mot de passe haché
         return internal_value    
     
