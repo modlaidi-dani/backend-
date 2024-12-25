3f3514103e1f14bfb2f3625686594b58508c589d
diva <95laidi@gmail.com>
2024-12-25 16:16:27 +0100

    pagination

--- erp/produits/views.py
+++ erp/produits/views.py
@@ -23,7 +23,6 @@ class CategoryViewset(viewsets.ModelViewSet):
     authentication_classes=[JWTAuthentication] 
     permission_classes=[IsAuthenticated ]
     filter_backends=[ UserFilterBackend, StoreFilter]
-    pagination_class = PageNumberPagination 
 
 class ProductViewset(viewsets.ModelViewSet):
     queryset=Product.objects.all()
