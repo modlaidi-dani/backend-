4fe12898b5162c23ec8e2116ddfcd773d6531cf9
diva <95laidi@gmail.com>
2024-12-29 14:54:57 +0100

    pipss

--- erp/core/filters.py
+++ erp/core/filters.py
@@ -22,5 +22,6 @@ class StoreFilter(DjangoFilterBackend):
                 return queryset.filter(store=costumeruser.EmployeeAt)
             elif request.session.get('store'):
                 return queryset.filter(store=request.session.get('store'))
+            return queryset
         except:
-            return []
+            return queryset
